1933853b322982464c1641c1dcd4fe9b21d51ee0
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-09 22:25:57 -0300

    Added Notifications

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,16 +3,34 @@ import {
   Text, Box, Image, Button,
 } from '@chakra-ui/react';
 import { MdOutlineAddShoppingCart, MdOutlineDone } from 'react-icons/md';
+import { useToast } from '@chakra-ui/toast';
+import Notification from '../../Notification';
 
 const Product = (props) => {
+  const toast = useToast();
   const {
-    product, addToCart, productIsOnCart, removeItem,
+    product, addToCart, productIsOnCart, removeItem, maxItemsOnCart, totalItems,
   } = props;
+
+  const handleAddToCart = () => {
+    if (totalItems < maxItemsOnCart) {
+      addToCart(product.id, 1);
+    } else {
+      const maxItemsOnCartId = 'max-items';
+
+      if (!toast.isActive(maxItemsOnCartId)) {
+        Notification(
+          'Error al agregar el producto.',
+          `No puedes agregar más de ${maxItemsOnCart} productos.`, maxItemsOnCartId,
+        );
+      }
+    }
+  };
 
   return (
     <>
       <Box
-        bg="#85C985"
+        bg="#2D7FA1"
         p="1rem"
         w="17rem"
         borderRadius="9px"
@@ -25,7 +43,7 @@ const Product = (props) => {
       >
         <Box
           boxShadow="lg"
-          bg="#537D53"
+          bg="#658FA1"
           borderRadius="7px"
           py={3}
           display="flex"
@@ -39,15 +57,16 @@ const Product = (props) => {
         </Box>
         <Box mt={4} display="flex" justifyContent="space-between">
           <Box>
-            <Text fontSize="18px" fontWeight="500">{product.name}</Text>
+            <Text color="#fff" fontSize="18px" fontWeight="500">{product.name}</Text>
             <Text
+              color="#fff"
               dangerouslySetInnerHTML={{ __html: product.description }}
               mt={1}
             />
           </Box>
-          <Text fontSize="18px" fontWeight="bold">{product.price.formatted_with_symbol}</Text>
+          <Text color="#fff" fontSize="18px" fontWeight="bold">{product.price.formatted_with_symbol}</Text>
         </Box>
-        <Button fontSize="15px" boxShadow="lg" disabled={productIsOnCart(product.id) ? 'true' : ''} mt={4} w="100%" onClick={() => (productIsOnCart(product.id) ? removeItem(product.id) : addToCart(product.id, 1))} leftIcon={productIsOnCart(product.id) ? <MdOutlineDone fontSize={20} /> : <MdOutlineAddShoppingCart fontSize={20} />}>{productIsOnCart(product.id) ? 'Agregado a tu carrito' : 'Agregar al carrito'}</Button>
+        <Button fontSize="15px" boxShadow="lg" disabled={productIsOnCart(product.id) ? 'true' : ''} mt={4} w="100%" onClick={() => (productIsOnCart(product.id) ? removeItem(product.id) : handleAddToCart())} leftIcon={productIsOnCart(product.id) ? <MdOutlineDone fontSize={20} /> : <MdOutlineAddShoppingCart fontSize={20} />}>{productIsOnCart(product.id) ? 'Agregado a tu carrito' : 'Agregar al carrito'}</Button>
       </Box>
     </>
   );
