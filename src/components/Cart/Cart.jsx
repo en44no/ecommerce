9be54bb95c4f67780d68648fbf4c8c14b6dd2205
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-09 22:24:19 -0300

    Price, empty cart and finish order are now fixed on bottom

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,7 +7,7 @@ import CartItem from './CartItem/CartItem';
 
 const Cart = (props) => {
   const {
-    cart, maxSameItemsOnCart, updateCart, emptyCart, removeItem,
+    cart, maxSameItemsOnCart, updateCart, emptyCart, removeItem, maxItemsOnCart, totalItems,
   } = props;
   const isEmpty = !cart.line_items.length;
   const text = cart.line_items.map((item) => `\n * ${item.quantity} ${item.name} - ${item.line_total.formatted_with_symbol}\n`).concat(`\n T O T A L: ${cart.subtotal.formatted_with_symbol}`);
@@ -22,25 +22,29 @@ const Cart = (props) => {
               maxSameItemsOnCart={maxSameItemsOnCart}
               updateCart={updateCart}
               removeItem={removeItem}
+              maxItemsOnCart={maxItemsOnCart}
+              totalItems={totalItems}
             />
           </>
         ))}
       </Box>
-      <Box>
-        <Text fontSize="lg" fontWeight="bold" textAlign="right">
-          Total:
-          {` ${cart.subtotal.formatted_with_symbol}`}
-        </Text>
-      </Box>
-      <Box mt={3} position="relative" bottom="0" display="flex" justifyContent="space-between">
-        <Button boxShadow="lg" onClick={emptyCart}>
-          Vaciar carrito
-        </Button>
-        <Link isExternal href={`https://wa.me/59894018406?text=${encodeURIComponent(`Hola! Quisiera pedir: ${text}`)}`}>
-          <Button leftIcon={<BsWhatsapp size="1.1rem" />} boxShadow="lg" colorScheme="whatsapp">
-            Terminar pedido
+      <Box w="104%" ml={-2} position="sticky" bottom={-2} bg="#fff" borderBottomLeftRadius="9px" borderBottomEndRadius="9px" p={4}>
+        <Box>
+          <Text fontSize="lg" fontWeight="bold" textAlign="right">
+            Total:
+            {` ${cart.subtotal.formatted_with_symbol}`}
+          </Text>
+        </Box>
+        <Box mt={3} position="relative" bottom="0" display="flex" justifyContent="space-between">
+          <Button boxShadow="lg" onClick={emptyCart}>
+            Vaciar carrito
           </Button>
-        </Link>
+          <Link isExternal href={`https://wa.me/59894018406?text=${encodeURIComponent(`Hola! Quisiera pedir: ${text}`)}`}>
+            <Button leftIcon={<BsWhatsapp size="1.1rem" />} boxShadow="lg" colorScheme="blue" bg="#658FA1">
+              Terminar pedido
+            </Button>
+          </Link>
+        </Box>
       </Box>
     </>
   );
