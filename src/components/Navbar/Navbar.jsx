06efab64edf17ffa1ad424a1d4d7faa50044df1e
Nahuel Márquez <57118285+en44no@users.noreply.github.com>
2021-11-12 22:56:18 -0300

    Added search products input

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
 import {
-  Box, Text,
+  Box, Text, Grid,
 } from '@chakra-ui/react';
+import Search from './Search';
 
-const Navbar = () => (
-  <>
-    <Box
-      as="nav"
-      h="5rem"
-      top="0"
-      left="0"
-      right="0"
-      zIndex="100"
-      position="fixed"
-    >
+const Navbar = (props) => {
+  const { searchProduct } = props;
+
+  return (
+    <>
       <Box
-        p={5}
-        w="100%"
-        bg="#2D7EA0"
-        pt={5}
-        boxShadow="lg"
+        as="nav"
+        h="5rem"
+        top="0"
+        left="0"
+        right="0"
+        zIndex="100"
+        position="fixed"
       >
-        <Box ml={10}>
-          <Text fontSize="xl" color="#fff" fontWeight="bold">Almacén</Text>
+        <Box
+          p={5}
+          w="100%"
+          bg="#2D7EA0"
+          pt={5}
+          boxShadow="lg"
+          display="flex"
+        >
+          <Grid templateColumns="repeat(5, 1fr)" gap={6} w="100%">
+            <Box w="100%" h="10">
+              <Text fontSize="xl" color="#fff" fontWeight="bold" mt={1} ml={10}>Almacén</Text>
+            </Box>
+            <Box />
+            <Box w="100%" h="10">
+              <Search searchProduct={searchProduct} w="100%" />
+            </Box>
+            <Box />
+          </Grid>
         </Box>
       </Box>
-    </Box>
-  </>
-);
+    </>
+  );
+};
 
 export default Navbar;
